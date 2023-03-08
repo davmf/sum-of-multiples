4e12b87aa42e59ce5128cfb691c42dbf65804267
David Fyfe <david@davidfyfe.net>
2023-03-08 22:13:26 +1100

    All tests pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    (1..=limit)
+    (1..limit)
         .filter(|i| is_multiple_of(i, factors))
         .sum()
 }
 
 fn is_multiple_of(n: &u32, factors: &[u32]) -> bool {
     for i in factors {
-        if n % i == 0 {
+        if *i != 0 && n % i == 0 {
             return true;
         }
     }
